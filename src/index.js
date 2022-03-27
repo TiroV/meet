f016a135e462bc727a7a35452d957361cc5e7c56
TiroV <91702935+TiroV@users.noreply.github.com>
2022-03-27 15:43:41 -0700

    Adds Atatus Functionality
    When commited, Integrates Atatus for testing purposes into the index.js file.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import * as atatus from 'atatus-spa';
+atatus.config('ad713cb76194416d91368845d001ef8e').install();
 
 ReactDOM.render(
   <React.StrictMode>
@@ -21,3 +23,6 @@ serviceWorkerRegistration.unregister();
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+atatus.notify(new Error('Test Atatus Setup'));
