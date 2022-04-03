c42b9d414de1c8b8caa28182d95e0027db529ea9
TiroV <91702935+TiroV@users.noreply.github.com>
2022-04-03 16:35:22 -0700

    Added images, added offline capabilities, added welcome screen and began process for google verification
    When committed, adds different icons, updates app to include the welcome screen and updates the index.js  for the serviceWorkerRegistration

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import React, { Component } from 'react';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from "./NumberOfEvents";
-import { getEvents, extractLocations } from './api';
+import WelcomeScreen from './WelcomeScreen';
+import { getEvents, extractLocations, checkToken, getAccessToken } from
+  './api';
 import './nprogress.css';
 import "./App.css";
+
 
 class App extends Component {
   state = {
     events: [],
     locations: [],
     numberOfEvents: 32,
+    showWelcomeScreen: undefined
   }
 
   updateEvents = (location) => {
@@ -25,20 +29,32 @@ class App extends Component {
     });
   }
 
-  componentDidMount() {
+  async componentDidMount() {
     this.mounted = true;
-    getEvents().then((events) => {
-      if (this.mounted) {
-        this.setState({ events, locations: extractLocations(events) });
-      }
-    });
+    const accessToken = localStorage.getItem('access_token');
+    const isTokenValid = (await checkToken(accessToken)).error ? false :
+      true;
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = searchParams.get("code");
+    this.setState({ showWelcomeScreen: !(code || isTokenValid) });
+    if ((code || isTokenValid) && this.mounted) {
+      getEvents().then((events) => {
+        if (this.mounted) {
+          this.setState({ events, locations: extractLocations(events) });
+        }
+      });
+    }
   }
+
 
   componentWillUnmount() {
     this.mounted = false;
   }
 
   render() {
+    if (this.state.showWelcomeScreen === undefined) return <div
+      className="App" />
+
     return (
       <div className="App">
         <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
@@ -46,6 +62,10 @@ class App extends Component {
         <NumberOfEvents
           updateNumberOfEvents={(number) => {
             this.updateNumberOfEvents(number);
+            <WelcomeScreen showWelcomeScreen={this.state.showWelcomeScreen}
+              getAccessToken={() => { getAccessToken() }} />
+
+
           }}
         />
       </div>
